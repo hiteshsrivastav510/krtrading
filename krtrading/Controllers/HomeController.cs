8cd47ef47e506040d0059c3e8b96680752319256
Bhanu <bhagwatisanga0601@gmail.com>
2022-11-26 10:58:10 +0530

    Design Admin and website panel.

--- krtrading/Controllers/HomeController.cs
+++ krtrading/Controllers/HomeController.cs
@@ -15,17 +15,21 @@ namespace krtrading.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page .";
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
             return View();
         }
 
 
-
-
-        public ActionResult Contact()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
